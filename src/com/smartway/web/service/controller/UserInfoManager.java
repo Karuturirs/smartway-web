96d55473db85edcf1ed34dd736650675a1f20ea7
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2016-07-18 10:01:28 +0530

    updated the userinfo manager

--- src/com/smartway/web/service/controller/UserInfoManager.java
+++ src/com/smartway/web/service/controller/UserInfoManager.java
@@ -1,14 +1,8 @@
 package com.smartway.web.service.controller;
 
-import java.io.IOException;
 import java.util.Collection;
 
-import javax.xml.bind.JAXBException;
-
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,18 +21,30 @@ public class UserInfoManager {
 	
 	@Autowired
 	GenericService userInfoService ;
-	@Autowired
 	GenericService userAuthService ;
 	/*private GsonBuilder gsonBuilder = new GsonBuilder();
     private Gson gson;*/
+	JSONObject error = new JSONObject();
     
 	@RequestMapping(value = "/getall", method = RequestMethod.GET, produces= "application/json")
-	public @ResponseBody JSONObject getAllUserInfo() throws JsonParseException, JsonMappingException, JAXBException, IOException, ParseException{
-		Collection<UserInfo> userInfos = (Collection<UserInfo>) userInfoService.getAll();
-		UserInfoMaster userinfomaster = new UserInfoMaster();
-		userinfomaster.setDataList(userInfos);
-		//Common common = new Common();
-		return 	new Common().pojo2Json(userinfomaster);
+	public @ResponseBody JSONObject getAllUserInfo(){
+		try{
+			System.out.println("-$-----");
+			Collection<UserInfo> userInfos = (Collection<UserInfo>) userInfoService.getAll();
+			UserInfoMaster userinfomaster = new UserInfoMaster();
+			System.out.println(userInfos.size());
+			
+			userinfomaster.setDataList(userInfos);
+			//Common common = new Common();
+			JSONObject outputJson=(new Common()).pojo2Json(userinfomaster);
+			return outputJson;
+		}catch(Exception e){
+			System.out.println("--"+e.getMessage());
+			error.put("ERROR", e.getMessage().toString());
+			e.printStackTrace();
+			return error;
+		}
+		
 	}
 	
 	
